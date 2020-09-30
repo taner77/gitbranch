2a5641c365d07f037f736c9a11265338866942a6
taner77 <tcelikim@gmail.com>
2020-09-30 12:27:00 -0400

    first update

--- src/test/java/BranchDeneme.java
+++ src/test/java/BranchDeneme.java
@@ -1,5 +1,7 @@
 public class BranchDeneme {
     public static void main(String[] args) {
         System.out.println("Branch deneme");
+        System.out.println("Branch deneme 2");
+        System.out.println("Calismalarimizi local feature'de yapiyoruz");
     }
 }
